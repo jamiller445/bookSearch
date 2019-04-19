8004b8bc36d3880b4a659cdcb163b46f8cd98a11
jeffrey.miller445 <jeffrey.miller445@gmail.com>
2019-04-18 23:10:34 -0400

    heroku-5

---
+++ build/precache-manifest.edb6fcf0cbac1caf48b353c7bca91fff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7ecc4302fe89befa2690",
+    "url": "/static/js/main.71ec410d.chunk.js"
+  },
+  {
+    "revision": "2ea4671e7f4c7b73ac48",
+    "url": "/static/js/2.6c6f4fa9.chunk.js"
+  },
+  {
+    "revision": "7ecc4302fe89befa2690",
+    "url": "/static/css/main.eaa4c1be.chunk.css"
+  },
+  {
+    "revision": "22977bba78629953f34bb29e8c024665",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bookSearch/precache-manifest.c6efc0b2a8d29e665bc422071661a73b.js"
+  "/precache-manifest.edb6fcf0cbac1caf48b353c7bca91fff.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/bookSearch/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
