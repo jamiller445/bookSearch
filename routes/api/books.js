6de5d82a59fefa2e55482e75316a867abc4922a7
jeffrey.miller445 <jeffrey.miller445@gmail.com>
2019-04-19 14:59:19 -0400

    heroku.19Apr19.10

--- routes/api/books.js
+++ routes/api/books.js
@@ -3,13 +3,14 @@ const booksController = require("../../controllers/booksController");
 
 // Matches with "/api/books"
 // router.route("/")
-router.route("/")
+router.route("/books")
   .get(booksController.findAll)
   .post(booksController.create);
 
 // Matches with "/api/books/:id"
 router
-  .route("/:id")
+  // .route("/:id")
+  .route("/books/:id")
   .get(booksController.findById)
   .put(booksController.update)
   .delete(booksController.remove);
--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const bookRoutes = require("./books");
 
 // Book routes
-router.use("/api/books", bookRoutes);
+// router.use("/api/books", bookRoutes);
+router.use("/api", bookRoutes);
 
 module.exports = router;
